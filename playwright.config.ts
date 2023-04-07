33008483c01395af616ff3766979379b174b1e35
nunch <ktojong@gmail.com>
2023-04-07 13:18:23 +0900

    feat: playwright global timeout 시간 조정

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  globalTimeout: 15000,
   timeout: 15000,
   testDir: './e2e',
   /* Run tests in files in parallel */
@@ -26,6 +27,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    actionTimeout: 10 * 1000,
+    navigationTimeout: 15 * 1000,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
