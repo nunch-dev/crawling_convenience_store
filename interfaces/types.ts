7846af738fc6c824ec49b690dae5529c54d052e1
nunch <ktojong@gmail.com>
2023-04-07 13:25:38 +0900

    refact: 미사용 type 삭제

--- interfaces/types.ts
+++
@@ -1,7 +0,0 @@
-export type ConvenienceType = 'CU' | 'SEVEN' | 'GS';
-
-export const ConvenienceType = {
-  CU: 'CU' as ConvenienceType,
-  SEVEN: 'SEVEN' as ConvenienceType,
-  GS: 'GS' as ConvenienceType,
-} as const;
