418dc789b74ef55691d610a298d1f3079ec3e5f7
nunch <ktojong@gmail.com>
2023-05-21 23:50:10 +0900

    feat: SEVEN 데이터 수집방식 변경

--- src/SEVEN.ts
+++ src/SEVEN.ts
@@ -1,320 +1,402 @@
 import { Crawlable } from './crawlable';
-import { Prisma } from '@prisma/client';
-import { chromium, Locator, Page } from '@playwright/test';
+import axios from 'axios';
+import { load } from 'cheerio';
+import * as fs from 'fs';
 
-export class SEVEN extends Crawlable<Prisma.CrawlingSevenCreateManyInput> {
+interface SevenGoods {
+  barcode: string | null;
+  name: string;
+  price: number;
+  img: string;
+  eventType: string;
+  isNew: boolean;
+
+  giftName?: string;
+  giftPrice?: number;
+  giftImg?: string;
+  giftBarcode?: string | null;
+  originalPrice?: number | null;
+  createdAt?: Date;
+
+  scrapId?: string;
+}
+
+export class SEVEN extends Crawlable<SevenGoods> {
   private readonly IMAGE_PATH = 'https://www.7-eleven.co.kr';
-  private readonly eventURL =
+  private readonly FIRST_CRAWLING_URL =
     'https://www.7-eleven.co.kr/product/presentList.asp';
+  private readonly AJAX_CALL_URL =
+    'https://www.7-eleven.co.kr/product/listMoreAjax.asp';
+  private readonly DEFAULT_IMAGE_URL = '/front/img/product/product_list_01.jpg';
 
-  constructor() {
-    super();
-  }
+  private readonly category = {
+    1: '1+1',
+    2: '2+1',
+    3: '덤증정',
+    4: '할인',
+  };
 
-  async init() {
-    this.browser = await chromium.launch({ headless: true });
-  }
+  private newProducts: string[] = [];
 
   async run() {
-    await this.init();
-
-    const result: Prisma.CrawlingSevenCreateManyInput[] = [];
-
-    const opo = await this.getOnePlusOneItems();
-    result.push(...opo);
-    const tpo = await this.getTwoPlusOneItems();
-    result.push(...tpo);
-
-    const gift = await this.getGiftItems();
-    result.push(...gift);
-
-    const sales = await this.getSalesItems();
-    result.push(...sales);
-
-    await this.browser.close();
-    return result;
+    await this.scrapeNewProducts();
+    return await this.scrapeList();
   }
 
-  private async clickTab(page: Page, tab: string) {
-    await page.evaluate((tab) => (window as any).fncTab(tab), tab);
-    await page.waitForLoadState('load');
-  }
-
-  private async getOnePlusOneItems() {
-    const context = await this.browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto(this.eventURL);
-    await page.waitForLoadState('load');
-
-    console.log('1+1 크롤링 시작');
-    await this.clickTab(page, '1');
-
-    console.log('아이템 로드 중...');
-    await this.loadAllItems(page);
-    console.log('아이템 로드 완료');
-
-    console.log('데이터 수집 시작');
-    const result = await this.crawlBasic(page);
-    await context.close();
-    console.log('데이터 수집 완료');
-    return result;
-  }
-
-  private async getTwoPlusOneItems() {
-    const context = await this.browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto(this.eventURL);
-    await page.waitForLoadState('load');
-
-    console.log('2+1 크롤링 시작');
-    await this.clickTab(page, '2');
-
-    console.log('아이템 로드 중...');
-    await this.loadAllItems(page);
-    console.log('아이템 로드 완료');
-
-    console.log('데이터 수집 시작');
-    const result = await this.crawlBasic(page);
-    await context.close();
-    console.log('데이터 수집 완료');
-    return result;
-  }
-
-  private async getGiftItems() {
-    const context = await this.browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto(this.eventURL);
-    await page.waitForLoadState('load');
-
-    console.log('증정행사 크롤링 시작');
-    await this.clickTab(page, '3');
-
-    console.log('아이템 로드 중...');
-    await this.loadAllItems(page);
-    console.log('아이템 로드 완료');
-
-    console.log('데이터 수집 시작');
-    const result = await this.crawlGift(page);
-    await context.close();
-    console.log('데이터 수집 완료');
-    return result;
-  }
-
-  private async getSalesItems() {
-    const context = await this.browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto(this.eventURL);
-    await page.waitForLoadState('load');
-
-    console.log('할인행사 크롤링 시작');
-    await this.clickTab(page, '4');
-
-    console.log('아이템 로드 중...');
-    await this.loadAllItems(page);
-    console.log('아이템 로드 완료');
-
-    console.log('데이터 수집 시작');
-    const result = await this.crawlSales(page);
-    await context.close();
-    console.log('데이터 수집 완료');
-    return result;
-  }
-
-  private async loadAllItems(page: Page) {
-    let prevCount;
-
-    while (true) {
-      // moreBtn click
-      try {
-        const moreBtn = await page.locator('.btn_more').locator('a');
-        await moreBtn.click();
-        await page.waitForTimeout(1000);
-      } catch {
-        break;
-      }
-
-      // check
-      try {
-        const currentValue = await page.locator('#listCnt').inputValue();
-        if (prevCount === currentValue) break;
-        prevCount = currentValue;
-      } catch {
-        break;
-      }
-    }
-  }
-
-  private async getProduct(
-    item: Locator
-  ): Promise<
-    Pick<
-      Prisma.CrawlingSevenCreateManyInput,
-      'name' | 'price' | 'img' | 'barcode'
-    >
-  > {
-    const name = await item
-      .locator('.pic_product')
-      .locator('.infowrap')
-      .locator('.name')
-      .innerText();
-    let priceString = await item
-      .locator('.pic_product')
-      .locator('.infowrap')
-      .locator('.price')
-      .innerText();
-    priceString = priceString.replace(/[,원]/g, '');
-    const price = Number(priceString);
-
-    let img = await item
-      .locator('.pic_product')
-      .locator('img')
-      .getAttribute('src');
-    let barcode = img?.split('.')[0].replace(/[\/\D]+/g, '') || '';
-    img = this.IMAGE_PATH + img;
-
-    return {
-      name,
-      price,
-      img,
-      barcode,
-    };
-  }
-
-  private async crawlBasic(page: Page) {
-    const arr: Prisma.CrawlingSevenCreateManyInput[] = [];
-
-    const items = await page.locator('#listUl > li').all();
-
-    const eventType = await items[0].innerText();
-
-    for (let i = 1; i < items.length; i++) {
-      const item = items[i];
-
-      try {
-        const { name, price, img, barcode } = await this.getProduct(item);
-        const obj: Prisma.CrawlingSevenCreateManyInput = {
-          name,
-          price,
-          eventType,
-          img,
-          barcode,
-        };
-        arr.push(obj);
-      } catch {
-        console.log(eventType, `${i}번째 아이템 에러 발생`);
-      }
-    }
-
-    return arr;
-  }
-
-  private async crawlGift(page: Page) {
-    const arr: Prisma.CrawlingSevenCreateManyInput[] = [];
-
-    const items = await page.locator('#listUl > li').all();
-
-    for (let i = 1; i < items.length; i++) {
-      const group = items[i];
-
-      try {
-        // 아이템이 더보기 버튼이면 종료
-        await group.locator('.btn_more');
-      } catch {
-        break;
-      }
-
-      const item = await group.locator('.btn_product_01');
-      const gift = await group.locator('.btn_product_02');
-
-      try {
-        // item
-        const { name, price, img, barcode } = await this.getProduct(item);
-        // gift
-        const {
-          name: giftName,
-          price: giftPrice,
-          img: giftImg,
-          barcode: giftBarcode,
-        } = await this.getProduct(gift);
-
-        const obj: Prisma.CrawlingSevenCreateManyInput = {
-          name,
-          price,
-          eventType: '덤증정',
-          img,
-          barcode,
-          giftName,
-          giftPrice,
-          giftImg,
-          giftBarcode: giftBarcode === '01' ? null : giftBarcode, // 01은 에러
-        };
-        arr.push(obj);
-      } catch {
-        console.log(`덤증정, ${i}번째 아이템 에러 발생`);
-      }
-    }
-
-    return arr;
-  }
-
-  private async crawlSales(page: Page) {
-    const arr: Prisma.CrawlingSevenCreateManyInput[] = [];
-
-    const items = await page.locator('#listUl > li').all();
-
-    await page.addScriptTag({
-      content: `fncGoView = (pCd) => {
-            $("#pCd").val(pCd);
-            $("#actFrm").attr("target", "_blank");
-            $("#actFrm").attr("action", "presentView.asp").submit();
-          }`,
+  // 신상품 체크
+  private async scrapeNewProducts() {
+    const resp = await axios.get(this.FIRST_CRAWLING_URL, {
+      params: {
+        cateCd1: '',
+        cateNm1: '',
+        cateCd2: '',
+        cateNm2: '',
+        cateCd3: '',
+        cateNm3: '',
+        pTab: 8, // 신상품
+        pCd: '',
+        initPageSize: '',
+      },
     });
 
-    for (let i = 1; i < items.length; i++) {
-      const item = items[i];
+    const result = await this.firstScraping(resp, '8');
+    result.forEach((v) => this.newProducts.push(v.scrapId!));
+  }
 
-      try {
-        const { name, price, img, barcode } = await this.getProduct(item);
-        const obj: Prisma.CrawlingSevenCreateManyInput = {
-          name,
-          price,
-          eventType: '할인',
-          img,
-          barcode,
+  private async scrapeList() {
+    const entries = Object.entries(this.category);
+    const result: SevenGoods[] = [];
+
+    for (let i = 0; i < entries.length; i++) {
+      const items = await this.scrapeType(entries[i]);
+      result.push(...items);
+    }
+
+    return result.map(({ scrapId, ...rest }) => rest);
+  }
+
+  private async scrapeType([categoryCode, eventType]: [string, string]) {
+    console.log(`${eventType} 상품 크롤링 중`);
+    const resp = await axios.get(this.FIRST_CRAWLING_URL, {
+      params: {
+        cateCd1: '',
+        cateNm1: '',
+        cateCd2: '',
+        cateNm2: '',
+        cateCd3: '',
+        cateNm3: '',
+        pTab: categoryCode,
+        pCd: '',
+        initPageSize: '',
+      },
+    });
+    let result;
+    if (categoryCode === '3') {
+      result = await this.firstScrapingGift(resp, categoryCode);
+    } else {
+      result = await this.firstScraping(resp, categoryCode);
+    }
+
+    if (categoryCode === '4') {
+      console.log('원가 크롤링 시작');
+      for (let i = 0; i < result.length; i++) {
+        result[i].originalPrice = await this.getOriginalPrice(
+          result[i].scrapId
+        );
+      }
+      console.log('원가 크롤링 종료');
+    }
+    console.log(`${eventType} 상품 크롤링 완료`);
+    return result.map((v) => ({
+      ...v,
+      eventType,
+    })) as SevenGoods[];
+  }
+
+  // 1+1 / 2+1 / 할인
+  private async firstScraping(resp: any, code: string) {
+    const $ = load(resp.data);
+    const $list = $('#listUl');
+    const $items = $list.find('> li').toArray();
+
+    const result: Omit<SevenGoods, 'eventType'>[] = [];
+
+    for (let i = 0; i < $items.length - 1; i++) {
+      if (i === 0) continue;
+
+      const el = $items[i];
+      const $item = $(el);
+
+      const name = $item.find('.pic_product > .infowrap > .name').text().trim();
+      const priceString = $item
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const price = Number(priceString.replace(/[,원]/g, ''));
+      const imgSrc = $item.find('.pic_product > img').attr('src');
+      const img = `${this.IMAGE_PATH}${imgSrc}`;
+      let barcode: string | null =
+        imgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === imgSrc) barcode = null;
+      const href = $item.find('a.btn_product_01').attr('href');
+      const scrapId = href?.replace(/[\/\D]+/g, '');
+
+      result.push({
+        barcode,
+        name,
+        price,
+        img,
+        isNew: this.newProducts.includes(scrapId || ''),
+        scrapId,
+      });
+    }
+
+    const hasMore = $list.find('.btn_more').is('li');
+    if (hasMore) {
+      let page = 2;
+      while (true) {
+        const params: Record<string, any> = {
+          intPageSize: 10,
+          pTab: code,
+          cateCd1: '',
+          cateCd2: '',
+          cateCd3: '',
         };
+        if (page > 2) {
+          params.intCurrPage = page;
+        }
+        const moreResp = await axios.get(
+          'https://www.7-eleven.co.kr/product/listMoreAjax.asp',
+          {
+            params,
+          }
+        );
 
-        const originalPrice = await this.getDetail(item, page);
-        obj.originalPrice = originalPrice === 0 ? price : originalPrice;
-
-        arr.push(obj);
-      } catch {
-        console.log(`할인상품 ${i}번째 아이템 에러 발생`);
+        const data = await this.secondScraping(moreResp);
+        const [items, isGoAhead] = data;
+        if (!isGoAhead) break;
+        result.push(...items);
+        page++;
       }
     }
 
-    return arr;
+    return result;
   }
 
-  private async getDetail(item: Locator, page: Page) {
-    const link = await item.getByRole('link');
-    const pagePromise = page.context().waitForEvent('page');
-    await link.click();
-    const detailPage = await pagePromise;
-    await detailPage.waitForLoadState();
+  // 1+1 / 2+1 / 할인
+  private async secondScraping(resp: any) {
+    const $ = load(resp.data);
+    const $items = $('li:not(.ico_tag_*)').toArray();
+    if ($items.length > 1) fs.writeFileSync('test.html', resp.data);
 
-    const originalPrice = await detailPage
-      .locator('.product_price')
-      .locator('del')
-      .innerText({ timeout: 1000 });
+    const result: Omit<SevenGoods, 'eventType'>[] = [];
+    for (let i = 0; i < $items.length - 1; i++) {
+      const el = $items[i];
+      const $item = $(el);
 
-    await detailPage.close();
+      const name = $item.find('.pic_product > .infowrap > .name').text().trim();
+      if (!name) continue;
+      const priceString = $item
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const price = Number(priceString.replace(/[,원]/g, ''));
+      const imgSrc = $item.find('.pic_product > img').attr('src');
+      const img = `${this.IMAGE_PATH}${imgSrc}`;
+      let barcode: string | null =
+        imgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === imgSrc) barcode = null;
+      const href = $item.find('a.btn_product_01').attr('href');
+      const scrapId = href?.replace(/[\/\D]+/g, '');
 
-    if (isNaN(Number(originalPrice))) return 0;
+      result.push({
+        barcode,
+        name,
+        price,
+        img,
+        isNew: this.newProducts.includes(scrapId || ''),
+        scrapId,
+      });
+    }
 
-    return Number(originalPrice);
+    const isGoAhead = $items.length > 1;
+    return [result, isGoAhead] as [Omit<SevenGoods, 'eventType'>[], boolean];
+  }
+
+  // 덤증정
+  private async firstScrapingGift(resp: any, code: string) {
+    const $ = load(resp.data);
+    const $list = $('#listUl');
+    const $items = $list.find('> li').toArray();
+
+    const result: Omit<SevenGoods, 'eventType'>[] = [];
+
+    for (let i = 0; i < $items.length - 1; i++) {
+      if (i === 0) continue;
+
+      const el = $items[i];
+      const $item = $(el).find('a.btn_product_01');
+      const itemHref = $item.attr('href');
+      const scrapId = itemHref?.replace(/[\/\D]+/g, '');
+      //NOTE 증정품은 신상품이 없다고 가정
+
+      const $gift = $(el).find('a.btn_product_02');
+
+      // 기본 상품
+      const name = $item.find('.pic_product > .infowrap > .name').text().trim();
+      const priceString = $item
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const price = Number(priceString.replace(/[,원]/g, ''));
+      const imgSrc = $item.find('.pic_product > img').attr('src');
+      const img = `${this.IMAGE_PATH}${imgSrc}`;
+      let barcode: string | null =
+        imgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === imgSrc) barcode = null;
+
+      // 증정품
+      const giftName = $gift
+        .find('.pic_product > .infowrap > .name')
+        .text()
+        .trim();
+      const giftPriceString = $gift
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const giftPrice = Number(giftPriceString.replace(/[,원]/g, ''));
+      const giftImgSrc = $gift.find('.pic_product > img').attr('src');
+      const giftImg = `${this.IMAGE_PATH}${giftImgSrc}`;
+      let giftBarcode: string | null =
+        giftImgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === giftImgSrc) giftBarcode = null;
+
+      result.push({
+        barcode,
+        name,
+        price,
+        img,
+        isNew: this.newProducts.includes(scrapId || ''),
+        giftName,
+        giftPrice,
+        giftImg,
+        giftBarcode,
+        scrapId,
+      });
+    }
+
+    const hasMore = $list.find('.btn_more').is('li');
+    if (hasMore) {
+      let page = 2;
+      while (true) {
+        const params: Record<string, any> = {
+          intPageSize: 10,
+          pTab: code,
+          cateCd1: '',
+          cateCd2: '',
+          cateCd3: '',
+        };
+        if (page > 2) {
+          params.intCurrPage = page;
+        }
+        const moreResp = await axios.get(
+          'https://www.7-eleven.co.kr/product/listMoreAjax.asp',
+          {
+            params,
+          }
+        );
+
+        const data = await this.secondScrapingGift(moreResp);
+        const [items, isGoAhead] = data;
+        if (!isGoAhead) break;
+        result.push(...items);
+        page++;
+      }
+    }
+
+    return result;
+  }
+
+  // 덤증정
+  private async secondScrapingGift(resp: any) {
+    const $ = load(resp.data);
+    const $items = $('li:not(.ico_tag_*)').toArray();
+    if ($items.length > 1) fs.writeFileSync('test.html', resp.data);
+
+    const result: Omit<SevenGoods, 'eventType'>[] = [];
+    for (let i = 0; i < $items.length - 1; i++) {
+      const el = $items[i];
+      const $item = $(el).find('a.btn_product_01');
+      const itemHref = $item.attr('href');
+      const scrapId = itemHref?.replace(/[\/\D]+/g, '');
+      //NOTE 증정품은 신상품이 없다고 가정
+
+      const $gift = $(el).find('a.btn_product_02');
+
+      // 기본 상품
+      const name = $item.find('.pic_product > .infowrap > .name').text().trim();
+      if (!name) continue;
+      const priceString = $item
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const price = Number(priceString.replace(/[,원]/g, ''));
+      const imgSrc = $item.find('.pic_product > img').attr('src');
+      const img = `${this.IMAGE_PATH}${imgSrc}`;
+      let barcode: string | null =
+        imgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === imgSrc) barcode = null;
+
+      // 증정품
+      const giftName = $gift
+        .find('.pic_product > .infowrap > .name')
+        .text()
+        .trim();
+      const giftPriceString = $gift
+        .find('.pic_product > .infowrap > .price')
+        .text()
+        .trim();
+      const giftPrice = Number(giftPriceString.replace(/[,원]/g, ''));
+      const giftImgSrc = $gift.find('.pic_product > img').attr('src');
+      const giftImg = `${this.IMAGE_PATH}${giftImgSrc}`;
+      let giftBarcode: string | null =
+        giftImgSrc?.split('.')[0].replace(/[\/\D]+/g, '') || null;
+      if (this.DEFAULT_IMAGE_URL === giftImgSrc) giftBarcode = null;
+
+      result.push({
+        barcode,
+        name,
+        price,
+        img,
+        isNew: this.newProducts.includes(scrapId || ''),
+        giftName,
+        giftPrice,
+        giftImg,
+        giftBarcode,
+        scrapId,
+      });
+    }
+
+    const isGoAhead = $items.length > 1;
+    return [result, isGoAhead] as [Omit<SevenGoods, 'eventType'>[], boolean];
+  }
+
+  // 원가 확인
+  private async getOriginalPrice(scrapId?: string) {
+    if (!scrapId) return;
+    const resp = await axios.get(
+      'https://www.7-eleven.co.kr/product/presentView.asp',
+      {
+        params: {
+          pCd: scrapId,
+        },
+      }
+    );
+
+    const $ = load(resp.data);
+    const originalPrice = $('.product_price del').text().replace(/\D+/g, '');
+    const discountPrice = $('.product_price strong').text().replace(/\D+/g, '');
+
+    return Number(originalPrice || discountPrice);
   }
 }
